bab425efbf31e9f0c08575fee383a1640c64482c
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-01-02 22:08:49 +0700

    navbar 7

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import { motion } from 'framer-motion';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+import useMediaQuery from '../hooks/useMediaQuery';
 
-const Landing = () => {
-  return <div>Landing</div>;
+const Landing = ({ setSelectedPage }) => {
+  const isAboveMediumScreens = useMediaQuery('(min-width: 1060px)');
+
+  return (
+    <section id="home" className="md:flex md:justify-between md:items-center md:h-full gap-16 py-10">
+      {/* IMAGE SECTION */}
+      <div className="md:order-2 flex justify-center basis-3/5 mt-16 md:mt-32">
+        {isAboveMediumScreens ? (
+          <div
+            className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20
+                before:rounded-t-[400px] before:w-full before:max-w-[400px] before:h-full
+                before:border-2 before:border-blue before:z-[-1]
+            "
+          >
+            <img
+              className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
+              src="/src/assets/profile-image.png"
+              alt="profile"
+            />
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default Landing;
