b5e66ab7b6c576316e501d24c1fa265c3da6816f
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-01-02 20:52:55 +0700

    navbar 4

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -30,9 +30,36 @@ const Navbar = ({ selectedPage, setSelectedPage }) => {
         {isAboveSmallScreens ? (
           <div className="flex justify-between gap-16 font-opensans text-sm font-semibold">
             <Link page="Home" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+            <Link page="Skill" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+            <Link page="Project" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+            <Link page="Testimonials" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+            <Link page="Contact" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
           </div>
         ) : (
-          <div></div>
+          <button className="rounded-full bg-red p-2" onClick={() => setIsMenuToggle(!isMenuToggled)}>
+            <img alt="menu-icon" src="../assets/menu-icon.svg" />
+          </button>
+        )}
+
+        {/* MOBILE MENU POPUP */}
+        {!isAboveSmallScreens && isMenuToggled && (
+          <div className="fixed right-0 bottom-0 h-full bg-blue w-[300px]">
+            {/* CLOSE ICON */}
+            <div className="flex justify-end p-12">
+              <button onClick={() => setIsMenuToggle(!isMenuToggled)}>
+                <img alt="close-icon" src="../assets/close-icon.svg" />
+              </button>
+            </div>
+
+            {/* MENU ITEMS */}
+            <div className="flex flex-col gap-10 ml-[33%] text-2xl text-deep-blue">
+              <Link page="Home" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+              <Link page="Skill" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+              <Link page="Project" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+              <Link page="Testimonials" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+              <Link page="Contact" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+            </div>
+          </div>
         )}
       </div>
     </nav>
