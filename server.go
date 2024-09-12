7d02e7932578fe4ae003008e526bce4b09b9240c
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-09-12 17:55:34 +0700

    go handler

--- server.go
+++ server.go
@@ -6,10 +6,17 @@ import (
 	"os"
 )
 
+func bookHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Here are the books")
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, World! I am a server.")
 	})
+
+	http.HandleFunc("/books", bookHandler)
+
 	fmt.Println("Server is running on port 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		fmt.Println("Error: ", err)
